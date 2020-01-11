f8b63d2dc32483377a8ed0a83150048881ec94f9
Meagan Utsey <meagan.utsey@gmail.com>
2020-01-11 03:43:51 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,5 +1,5 @@
 email_addresses = "john@doe.com, person@somewhere.org"
 parser = EmailAddressParser.new(email_addresses)
- 
-parser.parse
+attr_accessor:
+parser.parse.new
 # => ["john@doe.com", "person@somewhere.org"]
